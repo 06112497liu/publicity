cb9c6e0bde22bd43523f849e14fbbfc3678ed2b5
804912547@qq.com <804912547@qq.com>
2017-08-26 17:21:05 +0800

    bug修改

--- publicity-web/src/main/java/com/bbd/service/impl/ExportCmpStaReportServiceImpl.java
+++ publicity-web/src/main/java/com/bbd/service/impl/ExportCmpStaReportServiceImpl.java
@@ -25,6 +25,7 @@ import com.bbd.report.model.ReportElementModel;
 import com.bbd.report.model.TableDataModel;
 import com.bbd.service.IExportCmpStaReportService;
 import com.bbd.service.IReportService;
+import com.bbd.service.param.CmpStaReportPropVo;
 import com.bbd.service.param.CmpStaReportVo;
 import com.bbd.service.param.CompareReportVo;
 import com.bbd.util.NumUtils;
@@ -48,6 +49,8 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
     
     private static final Object[] title1 = new Object[]{"item", "itemValue"};
     
+    private static final Object[] title2 = new Object[]{"item", "num", "per"};
+    
     private static final Optional source = Optional.of("report/StatisticalReport.prpt");
 
     /**
@@ -55,12 +58,13 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
      */
     @Override
     public void cmpStaReport(OutputStream out) {
-
+        
+        // 报告元素集合
+        ArrayListMultimap<StructureEnum, ReportElementModel> elements = ArrayListMultimap.create();
+        
         ReportElementModel eleModel1 = new ReportElementModel(); // 公示异常企业区域列表分布
         ReportElementModel eleModel2 = new ReportElementModel(); // 公示异常企业区域条形图
-        List<CmpStaReportVo> list1 = getDistrictReport();
-        getArea();
-        Object[][] data1 = buildTwoArray(list1);
+        Object[][] data1 = buildTwoArray(getDistrictReport(1));
         TableDataModel dataModel1 = new TableDataModel(data1, title1);
         
         eleModel1.setName("RegionDistribute");
@@ -73,16 +77,65 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
         eleModel2.setElementEnum(ElementEnum.REPORT_DEFINITION_TABLE);
         eleModel2.setDataModel(dataModel1);
         
-        ArrayListMultimap<StructureEnum, ReportElementModel> elements = ArrayListMultimap.create();
+        // 分区域条形图
+        Map<String, Object[][]> areaData = getArea();
+        List<ReportElementModel> areaReportModel = Lists.newArrayList();
+        for(Map.Entry<String, Object[][]> entry : areaData.entrySet()) {
+            String key = entry.getKey();
+            System.out.println(key);
+            Object[][] o = entry.getValue();
+            ReportElementModel rm = new ReportElementModel();
+            TableDataModel tm = new TableDataModel(o, title1);
+            rm.setName(key);
+            rm.setDataName(key);
+            rm.setElementEnum(ElementEnum.REPORT_DEFINITION_TABLE);
+            rm.setDataModel(tm);
+            areaReportModel.add(rm);
+        }
+        
+        // 分区占比排行
+        ReportElementModel eleModel3 = new ReportElementModel();
+        Object[][] data3 = buildTwoArray(getDistrictReport(2));
+        TableDataModel model3 = new TableDataModel(data3, title1);
+        eleModel3.setName("DistrictRatio");
+        eleModel3.setDataName("DistrictRatio");
+        eleModel3.setElementEnum(ElementEnum.REPORT_DEFINITION_TABLE);
+        eleModel3.setDataModel(model3);
+        
+        // 行业占比排行
+        ReportElementModel eleModel4 = new ReportElementModel();
+        Object[][] data4 = buildTwoArray(getIndustryReport());
+        TableDataModel model4 = new TableDataModel(data4, title1);
+        eleModel4.setName("IndustryRatio");
+        eleModel4.setDataName("IndustryRatio");
+        eleModel4.setElementEnum(ElementEnum.REPORT_DEFINITION_TABLE);
+        eleModel4.setDataModel(model4);
+        
+        // 企业性质饼状图
+        ReportElementModel eleModel5 = new ReportElementModel();
+        Object[][] data5 = buildTwoArray(getCompanyPropertyReport());
+        TableDataModel model5 = new TableDataModel(data5, title2);
+        eleModel5.setName("CompanyProp");
+        eleModel5.setDataName("CompanyProp");
+        eleModel5.setElementEnum(ElementEnum.REPORT_DEFINITION_TABLE);
+        eleModel5.setDataModel(model5);
+        
+        
         elements.put(StructureEnum.REPORT_HEADER, eleModel1);
         elements.put(StructureEnum.REPORT_HEADER, eleModel2);
+        for(ReportElementModel e : areaReportModel) {
+            elements.put(StructureEnum.REPORT_HEADER, e);
+        }
+        elements.put(StructureEnum.REPORT_HEADER, eleModel3);
+        elements.put(StructureEnum.REPORT_HEADER, eleModel4);
+        elements.put(StructureEnum.REPORT_HEADER, eleModel5);
         
         ReportEngine re = new ReportEngine();
         re.generateReport(source, elements, null, ExportEnum.PDF, out);
     }
     
     // 获取贵阳市所有区域的公示异常企业数量
-    private List<CmpStaReportVo> getDistrictReport() {
+    private List<CmpStaReportVo> getDistrictReport(int type) {
         List<CmpStaReportVo> rs = Lists.newArrayList();
         List<CompareReportVo> ds = reportService.queryDistrictExInfos();
         sortList(ds, 1);
@@ -91,16 +144,20 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
         ds.forEach(p -> {
             CmpStaReportVo vo = new CmpStaReportVo();
             vo.setItem(p.getItemDesc());
-            vo.setItemvalue(p.getTotal());
+            if(type == 1) vo.setItemvalue(p.getTotal());
+            if(type ==2 ) vo.setItemvalue(NumUtils.trimPer(p.getPercent()));
             rs.add(vo);
         });
         return rs;
     }
     
+    // 区域分组
     private Map<String, Object[][]> getArea() {
         String[] strs = new String[]{"annual", "ins", "both", "total"};
         Map<String, Object[][]> rs = Maps.newHashMap();
         List<CompareReportVo> list = reportService.queryDistrictExInfos();
+        CompareReportVo whole = reportService.queryWholeCity();
+        list.add(whole);
         Map<String, List<CompareReportVo>> temp = list.stream().collect(Collectors.groupingBy(CompareReportVo::getItem));
         for(Map.Entry<String, List<CompareReportVo>> entry : temp.entrySet()) {
             List<CmpStaReportVo> areaList = Lists.newArrayList();
@@ -143,14 +200,15 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
     }
     
     // 获取贵阳市公示异常企业企业性质分布
-    private List<CmpStaReportVo> getCompanyPropertyReport() {
-        List<CmpStaReportVo> rs = Lists.newArrayList();
+    private List<CmpStaReportPropVo> getCompanyPropertyReport() {
+        List<CmpStaReportPropVo> rs = Lists.newArrayList();
         List<CompareReportVo> ds = reportService.queryCompanyPropertyExInfos();
         sortList(ds, 2);
         ds.forEach(p -> {
-            CmpStaReportVo vo = new CmpStaReportVo();
+            CmpStaReportPropVo vo = new CmpStaReportPropVo();
             vo.setItem(p.getItemDesc());
-            vo.setItemvalue(NumUtils.trimPer(p.getPercent()));
+            vo.setNum(p.getTotal());
+            vo.setPer(NumUtils.trimPer(p.getPercent()));
             rs.add(vo);
         });
         return rs;
@@ -172,7 +230,7 @@ public class ExportCmpStaReportServiceImpl implements IExportCmpStaReportService
         }
     }
     
- // 构建二维数组
+    // 构建二维数组
     private <T> Object[][] buildTwoArray(List<T> datas) {
         
         Integer rows = datas.size();
---
+++ publicity-web/src/main/java/com/bbd/service/param/CmpStaReportPropVo.java
@@ -0,0 +1,50 @@
+
+/** 
+ * BBD Service Inc 
+ * All Rights Reserved @2017 
+ */
+ package com.bbd.service.param; 
+/** 
+ * 比对统计分析报告对象
+ * @author liuweibo 
+ * @version $Id: CmpStaReportVo.java, v 0.1 2017年8月25日 下午1:55:14 liuweibo Exp $ 
+ */
+public class CmpStaReportPropVo {
+    
+    private String item;
+    
+    private Number num;
+    
+    private Double per;
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Number getNum() {
+        return num;
+    }
+
+    public void setNum(Number num) {
+        this.num = num;
+    }
+
+    public Double getPer() {
+        return per;
+    }
+
+    public void setPer(Double per) {
+        this.per = per;
+    }
+
+    @Override
+    public String toString() {
+        return "CmpStaReportPropVo [item=" + item + ", num=" + num + ", per=" + per + "]";
+    }
+    
+}
+
